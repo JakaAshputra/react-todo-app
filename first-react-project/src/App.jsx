b357e6cede8e3cf89559a16a23ca7d28aac3f171
JakaAshputra <jakaputrazz122@gmail.com>
2024-06-02 21:28:41 +0700

    Develop toggle todo feature

--- first-react-project/src/App.jsx
+++ first-react-project/src/App.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react'
-import './App.css'
-
+import Todos from './components/Todos'; 
 
 function App() {
   const [todos, setTodos] = useState([
@@ -23,15 +22,32 @@ function App() {
 
   console.log(todos)
 
+  const toggleCompleted = (todoId) => {
+    const updatedTodods = todos.map((todo) => {
+      if (todo.id === todoId) {
+        todo.completed = !todo.completed 
+      }
+      return todo
+    })
+    setTodos(updatedTodods)
+  }
+
   return (
-    <div>
-      <h1>My Todo List</h1>
-      {/* Gunakan method map di sini */}
-      {todos.map((todo) => {
-        return <p key={todo.id}>{todo.title}</p>
-      })}
+    <div style={styles.container}>
+      <h1 style={styles.title}>My Todo List</h1>
+      <Todos todos={todos} toggleCompleted={toggleCompleted}/>
     </div>
   )
+}
+
+const styles = {
+  container: {
+    textAlign: 'center',
+    padding: '12px',
+  },
+  title: {
+    fontSize: '36px',
+  },
 }
 
 export default App
---
+++ first-react-project/src/components/TodoItem.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+
+const TodoItem = ({ todo, toggleCompleted }) => {
+  // Definisikan function getTodoTitleStyle di sini
+  const getTodoTitleStyle = () => {
+    if (todo.completed === true) {
+      return { textDecoration: 'line-through' }
+    } else {
+      return { textDecoration: 'none' }
+    }
+  }
+
+  return (
+    <div style={styles.todoItem}>
+      {/* Menambahkan checkbox */}
+      <input 
+        type="checkbox" 
+        style={styles.checkbox} 
+        onChange={() => toggleCompleted(todo.id)}
+      />
+      {/* Panggil function getTodoTitleStyle */}
+      <p style={getTodoTitleStyle()}>{todo.title}</p>
+    </div>
+  )
+}
+
+const styles = {
+  todoItem: {
+    border: '2px solid #f4f4f4',
+    fontSize: '24px',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  checkbox: {
+    marginRight: '10px',
+    height: '18px',
+    width: '18px',
+  },
+}
+
+export default TodoItem
